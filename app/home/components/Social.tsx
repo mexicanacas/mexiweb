36f18149e4a5b7bbf4f8e416396ea7e580320137
Filip Gornitzka Abelson <fga@talkmore.no>
2025-07-08 23:07:39 +0200

    fix: update links with aria-label and clean up email anchor tag

--- app/home/components/Social.tsx
+++ app/home/components/Social.tsx
@@ -4,6 +4,7 @@ export const Social = () => {
       <a
         target="_blank"
         rel="noopener noreferrer"
+        aria-label="Instagram"
         href="https://www.instagram.com/mexicanacas?igsh=b3B5N3ZycnI0YzBx"
         className="hover:text-primary/80"
       >
@@ -12,6 +13,7 @@ export const Social = () => {
       <a
         target="_blank"
         rel="noopener noreferrer"
+        aria-label="Spotify"
         href="https://open.spotify.com/artist/2I4RX8u3uyBXG0kxvhzLui?si=AULMUxW9SGeEl_sQCOWy8Q"
         className="hover:text-primary/80"
       >
@@ -20,16 +22,13 @@ export const Social = () => {
       <a
         target="_blank"
         rel="noopener noreferrer"
+        aria-label="Facebook"
         href="https://www.facebook.com/losmexicanacas"
         className="hover:text-primary/80"
       >
         <i className="fa-brands fa-facebook-f" />
       </a>
-      <a
-        target="_blank"
-        href="mailto:booking@mexi.no"
-        className="hover:text-primary/80"
-      >
+      <a href="mailto:booking@mexi.no" className="hover:text-primary/80">
         <i className="fa-solid fa-envelope" />
       </a>
     </nav>
