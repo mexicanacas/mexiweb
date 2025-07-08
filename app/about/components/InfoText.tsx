ad91c29bab86e9f156708c0597eb2fed9b6aaf6d
Filip Gornitzka Abelson <fga@talkmore.no>
2025-07-08 21:48:27 +0200

    fix: update genre text and remove Trondheim department

--- app/about/components/InfoText.tsx
+++ app/about/components/InfoText.tsx
@@ -2,11 +2,17 @@ export const InfoText = () => (
   <section className="bg-mexi-black bg-opacity-50 p-4 md:max-w-md">
     <h1 className="text-2xl md:text-4xl">Om oss</h1>
     <p>
-      Vi er et band som spiller akustisk latin fusion-musikk - med utgangspunkt
-      i mariachi-kulturen fra Mexico. Bandet ble opprettet av studenter i
-      Trondheim i 2018 for å spille på en temafest og har siden den gang utvidet
-      repertoar og ambisjonsnivå. Nå finnes Los Mexicanacas både i Oslo og
-      Trondheim, holder konserter og lager fest ved alle slags anledninger.
+      Vi er et band som spiller akustisk latin fusion-musikk, inspirert av
+      rumba, flamenco, mariachi og reggaeton.
+    </p>
+    <p className="mt-1">
+      Det blir alltid fiesta når Los Mexicanacas inntar scenen!
+    </p>
+    <p className="mt-4">
+      Bandet ble opprettet av studenter i Trondheim i 2018 for å spille på en
+      temafest. Siden den gang har vi flyttet til Oslo, utvidet repertoaret og
+      ambisjonsnivået &ndash; og holder nå konserter og lager fest ved alle
+      slags anledninger!
     </p>
   </section>
 );
