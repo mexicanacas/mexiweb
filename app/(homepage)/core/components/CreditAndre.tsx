fd90ea1b52ece1d62de875521a834bb20e4245ce
johanbrun <johannicbrun@gmail.com>
2025-11-28 17:42:33 +0100

    remove leftovers form next

--- app/(homepage)/core/components/CreditAndre.tsx
+++
@@ -1,7 +0,0 @@
-export const CreditAndre = () => (
-  <span className="text-xs">
-    Fotokreditering
-    <a href="https://www.instagram.com/andrenesheim/">@andrenesheim</a> /{' '}
-    <a href="https://www.instagram.com/saltartmusic/">SALT</a>
-  </span>
-);
