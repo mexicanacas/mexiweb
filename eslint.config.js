9e007b192695525d3a99e6dada97a2c8344be37a
Filip Gornitzka Abelson <fga@talkmore.no>
2025-11-29 14:58:33 +0100

    chore: run format

--- eslint.config.js
+++ eslint.config.js
@@ -1,62 +1,62 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import astro from "eslint-plugin-astro";
-import prettier from "eslint-plugin-prettier";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import astro from 'eslint-plugin-astro';
+import prettier from 'eslint-plugin-prettier';
 
 // parsers
 const tsParser = tseslint.parser;
 const astroParser = astro.parser;
 
 export default defineConfig([
-  // Global configuration
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-    },
-  },
+	// Global configuration
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
 
-  // Base configs
-  js.configs.recommended,
-  tseslint.configs.recommended,
+	// Base configs
+	js.configs.recommended,
+	tseslint.configs.recommended,
 
-  // Prettier config
-  {
-    plugins: {
-      prettier: prettier,
-    },
-    rules: {
-      // disable warnings, since prettier should format on save
-      "prettier/prettier": "off",
-    },
-  },
+	// Prettier config
+	{
+		plugins: {
+			prettier: prettier
+		},
+		rules: {
+			// disable warnings, since prettier should format on save
+			'prettier/prettier': 'off'
+		}
+	},
 
-  // astro setup
-  astro.configs.recommended,
-  {
-    files: ["**/*.astro"],
-    languageOptions: {
-      parser: astroParser,
-      parserOptions: {
-        parser: tsParser,
-        extraFileExtensions: [".astro"],
-        sourceType: "module",
-        ecmaVersion: "latest",
-        project: "./tsconfig.json",
-      },
-    },
-    rules: {
-      "no-undef": "off", // Disable "not defined" errors for specific Astro types that are globally available (ImageMetadata)
-      "@typescript-eslint/no-explicit-any": "off", // you may want this as it can get annoying
-    },
-  },
+	// astro setup
+	astro.configs.recommended,
+	{
+		files: ['**/*.astro'],
+		languageOptions: {
+			parser: astroParser,
+			parserOptions: {
+				parser: tsParser,
+				extraFileExtensions: ['.astro'],
+				sourceType: 'module',
+				ecmaVersion: 'latest',
+				project: './tsconfig.json'
+			}
+		},
+		rules: {
+			'no-undef': 'off', // Disable "not defined" errors for specific Astro types that are globally available (ImageMetadata)
+			'@typescript-eslint/no-explicit-any': 'off' // you may want this as it can get annoying
+		}
+	},
 
-  // Ignore patterns
-  {
-    ignores: ["dist/**", "**/*.d.ts", ".github/", ".vercel/"],
-  },
+	// Ignore patterns
+	{
+		ignores: ['dist/**', '**/*.d.ts', '.github/', '.vercel/']
+	}
 ]);
