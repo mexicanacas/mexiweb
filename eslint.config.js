f0239b757812c9916d718ee14024aa7b81c69079
Filip Gornitzka Abelson <fga@talkmore.no>
2025-11-29 14:54:20 +0100

    feat: more config settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,62 @@
-import eslintPluginAstro from "eslint-plugin-astro";
-export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import astro from "eslint-plugin-astro";
+import prettier from "eslint-plugin-prettier";
+
+// parsers
+const tsParser = tseslint.parser;
+const astroParser = astro.parser;
+
+export default defineConfig([
+  // Global configuration
   {
-    rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
   },
-];
+
+  // Base configs
+  js.configs.recommended,
+  tseslint.configs.recommended,
+
+  // Prettier config
+  {
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      // disable warnings, since prettier should format on save
+      "prettier/prettier": "off",
+    },
+  },
+
+  // astro setup
+  astro.configs.recommended,
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".astro"],
+        sourceType: "module",
+        ecmaVersion: "latest",
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "no-undef": "off", // Disable "not defined" errors for specific Astro types that are globally available (ImageMetadata)
+      "@typescript-eslint/no-explicit-any": "off", // you may want this as it can get annoying
+    },
+  },
+
+  // Ignore patterns
+  {
+    ignores: ["dist/**", "**/*.d.ts", ".github/", ".vercel/"],
+  },
+]);
